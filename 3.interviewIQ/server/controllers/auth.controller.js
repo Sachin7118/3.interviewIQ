879d6272bd4ad3ffaf432146c82343fb1e3bf71a
Sachin7118 <kumarssachin112@gmail.com>
2026-08-29 20:23:25 +0530

    Update auth.controller.js

--- 3.interviewIQ/server/controllers/auth.controller.js
+++ 3.interviewIQ/server/controllers/auth.controller.js
@@ -10,8 +10,8 @@ const setAuthCookie = (res, userId) => {
     const token = genToken(userId)
     res.cookie("token", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
 }
